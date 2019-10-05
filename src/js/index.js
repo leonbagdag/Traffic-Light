8b1915881bf7e4582a030318c614a7a90055549b
leonbagdag <dsrt1991@gmail.com>
2019-10-05 01:35:58 +0000

    traffic

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,7 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { TrafficLight } from "./component/home.js";
+import { TrafficLight } from "./component/Traffic.js";
 
 //render your react application
 ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
